a5e4f8a951316f43e199f90aad7d8a0a37cbaf01
Ethan <erdai979@learner.dvusd.org>
2024-01-09 12:49:02 -0700

    Save and Open files

--- Menus/Menus/Form1.vb
+++ Menus/Menus/Form1.vb
@@ -10,4 +10,18 @@
     Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
 
     End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim outfile As New IO.StreamWriter("Out.txt")
+        outfile.Write(TextBox1.Text)
+        outfile.Close()
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        If IO.File.Exists("out.txt") Then
+            Dim infile As New IO.StreamReader("Out.txt")
+            TextBox1.Text = infile.ReadToEnd
+            infile.Close()
+        End If
+    End Sub
 End Class
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SY23-24
 Code for software class<br>
 <b>1/4/24</b>-setting up repository<br>
-<b>1/5/24</b>-Updating local
-<b>1/8/24</b>-Monday update
+<b>1/5/24</b>-Updating local<br>
+<b>1/8/24</b>-Monday update<br>
